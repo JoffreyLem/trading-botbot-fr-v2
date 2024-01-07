a985e37a991ba4e6a05c68c78d129ea041442dc9
joffrey <lemery.joffrey@outlook.fr>
2024-01-07 17:08:29 +0100

    update docker compose mariadb prod to good ip

--- StrategyApi.DataBase/ServiceDatabaseExtension.cs
+++ StrategyApi.DataBase/ServiceDatabaseExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using StrategyApi.DataBase.DbContext;
 using StrategyApi.DataBase.Repositories;
 
@@ -14,7 +15,16 @@ public static class ServiceDatabaseExtension
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         services.AddDbContext<StrategyContext>(options =>
-            options.UseMySql(connectionString, new MariaDbServerVersion(new Version())), ServiceLifetime.Singleton);
+            options
+                .UseMySql(connectionString, new MariaDbServerVersion(ServerVersion.AutoDetect(connectionString)),
+                    builder =>
+                        builder.EnableRetryOnFailure(
+                            2,
+                            TimeSpan.FromSeconds(10),
+                            null))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging(), ServiceLifetime.Singleton);
 
         services.AddSingleton<IStrategyFileRepository, StrategyFileRepository>();
 
